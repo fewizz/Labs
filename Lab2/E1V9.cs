bae159b14f7792b24a62dd7c9dc645fb286e71d8
fewizz <hazeevaidar@gmail.com>
2020-09-07 21:40:16 +0300

    Lab2_E1V9, Lab6_E2V1

---
+++ Lab2/E1V9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class E1V9
+    {
+        static void Main(string[] args)
+        {
+            double
+                begin = -7,
+                end = 3,
+                step = 0.25;
+
+            Console.WriteLine("|   x   |   y   |");
+
+            for (double x = begin; x <= end; x += step)
+            {
+                double y = double.NaN;
+                if (x >= -7 && x < -6)
+                    y = 2;
+                if (x >= -6 && x < -2)
+                    y = 0.25*(x + 2);
+                if (x >= -2 && x < 0)
+                    y = 2 - Math.Sqrt(-x*(4 + x));
+                if (x >= 0 && x < 2)
+                    y = Math.Sqrt(4 - x * x);
+                if (x >= 2 && x <= 3)
+                    y = -x + 2;
+
+                Console.WriteLine("|{0,7:F3}|{1,7:F3}|", x, y);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Lab6/E2V1.cs
+++ Lab6/E2V1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,56 @@ namespace Lab6
     {
         public static void Main(string[] args)
         {
-            
+            string path = "../../E2V1/";
+            string txt = new StreamReader(path+"names.txt").ReadToEnd();
+
+            List<string> persons =
+                txt.Split(
+                    new char[] { '\r', '\n' },
+                    StringSplitOptions.RemoveEmptyEntries
+                ).ToList();
+
+            int maxAge = -1;
+
+            foreach (string subs in persons)
+                maxAge = Math.Max(maxAge, int.Parse(subs.Split()[3]));
+
+            while(persons.Count > 0)
+            {
+                string secondNameToCheck = persons[0].Split()[0];
+                StreamWriter sw = null;
+
+                for (int x = 1; x < persons.Count;)
+                {
+                    string secondName = persons[x].Split()[0];
+                    if (secondNameToCheck.Equals(secondName))
+                    {
+                        if (sw == null)
+                        {
+                            sw = new StreamWriter(
+                                path + secondName + ".txt",
+                                true
+                            );
+                            sw.WriteLine(persons[0].Split()[0]);
+                        }
+
+                        sw.WriteLine(persons[x]);
+                        persons.RemoveAt(x);
+                    }
+                    else x++;
+                }
+                if(sw != null)
+                {
+                    sw.Flush();
+                    sw.Close();
+                }
+                persons.RemoveAt(0);
+            }
+
+            using (StreamWriter ma = new StreamWriter(path + "max_age.txt"))
+            {
+                ma.WriteLine(maxAge);
+            }
         }
     }
 }
